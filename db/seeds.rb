fe3f52e4d77f73c6deac15ba282a837aeefa7b28
paulotijero <paulotijero@gmail.com>
2019-04-09 10:07:56 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+movie_id = 1
+serie_id = 1
+
+100.times do
+  Movie.create(
+    title: "Mo - #{movie_id}",
+    description: "Description Mo - #{movie_id}",
+    rating: 0,
+    duration: rand(90..180),
+    price: rand(5..25),
+    status: ["coming_soon", "preorder", "billboard"].sample,
+    playback: 0
+  )
+  Serie.create(
+    title: "Se - #{serie_id}",
+    description: "Description Se - #{serie_id}",
+    rating: 0,
+    price: rand(5..25),
+    status: ["coming_soon", "preorder", "billboard"].sample
+  )
+  number_episodes = rand(10..30)
+  episode_id = 1
+  number_episodes.times do
+    Episode.create(
+      title: "Ep - #{episode_id} Se - #{serie_id}",
+      description: "Description Ep - #{episode_id} Se - #{serie_id}",
+      duration: rand(45..120),
+      playback: 0,
+      serie_id: serie_id
+    )
+    episode_id += 1
+  end
+  movie_id += 1
+  serie_id += 1
+end
+
+rentals_id = 1
+available_movies = Array(1..(movie_id-1))
+available_series = Array(1..(serie_id-1))
+100.times do
+  type = ["Movie", "Serie"].sample
+  rental = Rental.create(
+    paid_price: rand(5..50),
+    rentable_type: type,
+    rentable_id: type == "Movie" ? available_movies.sample : available_series.sample
+  )
+  type == "Movie" ? available_movies.delete(rental[:rentable_id]) : available_series.delete(rental[:rentable_id])
+  rentals_id += 1
+end
