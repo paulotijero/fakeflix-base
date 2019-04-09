25da45f2104893f2a9ffac22fb466e46f824fd90
paulotijero <paulotijero@gmail.com>
2019-04-09 09:36:58 -0500

    Add controller and test rentals

--- app/controllers/api/rentals_controller.rb
+++ app/controllers/api/rentals_controller.rb
@@ -1,5 +1,34 @@
 module Api
   class Api::RentalsController < ApplicationController
 
+    def index
+      case
+      when params[:rentable_type]
+        render json: Rental.where(rentable_type: params["rentable_type"])
+      else
+        render json: Rental.all
+      end
+    end
+
+    def movies
+      movie = Movie.find(params[:id])
+      rental_movie = movie.rentals.create(
+        paid_price: movie.price
+      )
+      render json: rental_movie, status: :created
+    end
+
+    def series
+      serie = Serie.find(params[:id])
+      rental_serie = serie.rentals.create(
+        paid_price: serie.price
+      )
+      render json: rental_serie, status: :created
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+
   end
 end
--- spec/controllers/api/rentals_controller_spec.rb
+++ spec/controllers/api/rentals_controller_spec.rb
@@ -2,4 +2,94 @@ require 'rails_helper'
 
 RSpec.describe Api::RentalsController, type: :controller do
 
+  before do
+    @movie = Movie.create(
+      title: "El titulo",
+      description: "La descripcion",
+      rating: 2,
+      duration: 5,
+      price: 100,
+      playback: 0,
+      status: "coming_soon"
+    )
+    @serie = Serie.create(
+      title: "El titulo",
+      description: "La descripcion",
+      rating: 2,
+      price: 100
+    )
+    @rental_serie = Rental.create(
+      paid_price: 10,
+      rentable_type: "Serie",
+      rentable_id: @serie.id
+    )
+    @rental_movie = Rental.create(
+      paid_price: 10,
+      rentable_type: "Movie",
+      rentable_id: @movie.id
+    )
+  end
+
+  describe 'GET index' do
+    it 'returns http status ok' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render json with all' do
+      get :index
+      rental = JSON.parse(response.body)
+      expect(rental.size).to eq 2
+    end
+    it 'returns http status ok with all rentable_type' do
+      get :index, params: { rentable_type: @rental_movie.rentable_type }
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render json with all rental-movies' do
+      get :index, params: { rentable_type: "Movie" }
+      rental = JSON.parse(response.body)
+      expect(rental.size).to eq 1
+    end
+    it 'render json with all rental-serie' do
+      get :index, params: { rentable_type: "Serie" }
+      rental = JSON.parse(response.body)
+      expect(rental.size).to eq 1
+    end
+  end
+
+  describe "POST movies" do
+    it "returns http status movies" do
+      post :movies, params: { 
+        id: @movie.id
+      }
+      expect(response.status).to eq(201)
+      expect(response).to have_http_status(:created)
+    end
+    it "returns the created movies" do
+      post :movies, params: { 
+        id: @movie.id
+      }
+      expected_rental_movie = JSON.parse(response.body)
+      expect(expected_rental_movie).to have_key("id")
+      expect(expected_rental_movie["paid_price"]).to eq(@movie.price)
+    end
+  end
+
+  describe "POST series" do
+    it "returns http status series" do
+      post :series, params: { 
+        id: @serie.id
+      }
+      expect(response.status).to eq(201)
+      expect(response).to have_http_status(:created)
+    end
+    it "returns the created series" do
+      post :series, params: { 
+        id: @serie.id
+      }
+      expected_rental_serie = JSON.parse(response.body)
+      expect(expected_rental_serie).to have_key("id")
+      expect(expected_rental_serie["paid_price"]).to eq(@serie.price)
+    end
+  end
+
 end
