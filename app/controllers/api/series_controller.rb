9a82552534d088cd21b4b6c614850f373b0c1ad1
paulotijero <paulotijero@gmail.com>
2019-04-08 23:14:58 -0500

    Add controller and test series

--- app/controllers/api/series_controller.rb
+++ app/controllers/api/series_controller.rb
@@ -1,5 +1,24 @@
 module Api
   class Api::SeriesController < ApplicationController
     
+    def index
+      render json: Serie.all
+    end
+
+    def show
+      render json: Serie.find(params[:id])
+    end
+
+    def rating
+      serie = Serie.find(params[:id])
+      serie.rating = params[:attributes][:rating]
+      serie.save
+      render json: serie
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+
   end
 end
--- spec/controllers/api/series_controller_spec.rb
+++ spec/controllers/api/series_controller_spec.rb
@@ -2,4 +2,63 @@ require 'rails_helper'
 
 RSpec.describe Api::SeriesController, type: :controller do
 
+  before do
+    @serie = Serie.create(
+      title: "El titulo",
+      description: "La descripcion",
+      rating: 2,
+      price: 100
+    )
+  end
+
+  describe 'GET index' do
+    it 'returns http status ok' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render json with all series' do
+      get :index
+      series = JSON.parse(response.body)
+      expect(series.size).to eq 1
+    end
+  end
+
+  describe 'GET show' do
+    it 'returns http status ok' do
+      get :show, params: { id: @serie }
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render the correct serie' do
+      get :show, params: { id: @serie }
+      expected_serie = JSON.parse(response.body)
+      expect(expected_serie["id"]).to eq(@serie.id)
+    end
+    it 'returns http status not found' do
+      get :show, params: { id: 'xxx' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "PATCH rating" do
+    it "returns http status ok" do
+      patch :rating, params: {
+        id: @serie.id,
+        attributes: {
+          rating: 5
+        }
+      }
+      expect(response).to have_http_status(:ok)
+    end
+    it "returns the update playback" do
+      patch :rating, params: {
+        id: @serie.id,
+        attributes: {
+          rating: 5
+        }
+      }
+      expected_playback = JSON.parse(response.body)
+      expect(expected_playback["rating"]).to eq(5)
+    end
+  end
+
 end
