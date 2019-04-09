3982b4513d4b278c9ef9665de7aa97640e96d397
paulotijero <paulotijero@gmail.com>
2019-04-08 23:15:37 -0500

    Add controller and test episodes

--- app/controllers/api/episodes_controller.rb
+++ app/controllers/api/episodes_controller.rb
@@ -1,5 +1,20 @@
 module Api
   class Api::EpisodesController < ApplicationController
+
+    def show
+      render json: Episode.find(params[:id])
+    end
+
+    def playback
+      episode = Episode.find(params[:id])
+      episode.playback = params[:attributes][:playback]
+      episode.save
+      render json: episode
+    end
     
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+
   end
 end
--- spec/controllers/api/episodes_controller_spec.rb
+++ spec/controllers/api/episodes_controller_spec.rb
@@ -2,4 +2,58 @@ require 'rails_helper'
 
 RSpec.describe Api::EpisodesController, type: :controller do
 
+  before do
+    @serie = Serie.create(
+      title: "El titulo",
+      description: "La descripcion",
+      rating: 2,
+      price: 100
+    )
+    @episode = Episode.create(
+      title: "El titulo",
+      description: "La descripcion",
+      duration: 5,
+      serie_id: @serie.id,
+      playback: 0
+    )
+  end
+  
+  describe 'GET show' do
+    it 'returns http status ok' do
+      get :show, params: { id: @episode }
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render the correct movie' do
+      get :show, params: { id: @episode }
+      expected_episode = JSON.parse(response.body)
+      expect(expected_episode["id"]).to eq(@episode.id)
+    end
+    it 'returns http status not found' do
+      get :show, params: { id: 'xxx' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "PATCH playback" do
+    it "returns http status ok" do
+      patch :playback, params: {
+        id: @episode.id,
+        attributes: {
+          playback: 50
+        }
+      }
+      expect(response).to have_http_status(:ok)
+    end
+    it "returns the update playback" do
+      patch :playback, params: {
+        id: @episode.id,
+        attributes: {
+          playback: 50
+        }
+      }
+      expected_playback = JSON.parse(response.body)
+      expect(expected_playback["playback"]).to eq(50)
+    end
+  end
+
 end
